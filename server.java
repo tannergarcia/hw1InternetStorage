ee0c7a35d1a1ec4fdc11fff9ba61e17035426571
Chris Newell <110441007+crnewell@users.noreply.github.com>
2025-02-23 16:01:55 -0500

    Added comments to server.java

--- server.java
+++ server.java
@@ -13,6 +13,7 @@ public class server {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is listening on port " + port);
 
+            // Server is always listening for new connections
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected");
@@ -34,29 +35,43 @@ class ClientHandler extends Thread {
     }
 
     public void run() {
+
+        // Initialize structures to read client input and write server output
         try (InputStream input = socket.getInputStream();
              BufferedReader reader = new BufferedReader(new InputStreamReader(input));
              OutputStream output = socket.getOutputStream();
              PrintWriter writer = new PrintWriter(output, true)) {
             writer.println("Hello!");
             String text;
+
+            // Loop to always check the input message from client
             while ((text = reader.readLine()) != null) {
                 System.out.println("Received: " + text);
+
+                // Alpha characters and spaces are allowed
                 if (!text.matches("[A-Za-z ]+")) {
                     writer.println("Invalid input: only letters are allowed. Please try again.");
                     System.out.println("Received non alphabet input");
                     continue;
                 }
+
+                // Disconnect if "bye" was sent by client
                 if (text.equalsIgnoreCase("bye")) {
                     writer.println("disconnected");
                     break;
                 }
+
+                // Send back capitalized input if valid
                 String capitalizedText = text.toUpperCase();
                 writer.println(capitalizedText);
             }
+
+
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
             ex.printStackTrace();
+
+        // Always closes the connection
         } finally {
             try {
                 socket.close();
